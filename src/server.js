2118daec63029186aafbd32df356cf08b07921b9
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-07-24 09:05:19 -0300

    master
    criado CORS

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 var PORT = process.env.PORT || 3000;
 app.get("/", function (req, res) {
+    console.log("entrou");
     res.send({ "fulfillmentMessages": "ESTA RODANDO NO HEROKU" });
 });
 app.listen(PORT, function () {
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 ﻿﻿const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -9,6 +11,7 @@ const PORT = process.env.PORT || 3000
 
 
 app.get("/", (req, res) => {
+    console.log("entrou")
     res.send({"fulfillmentMessages":"ESTA RODANDO NO HEROKU"})
 })
 app.listen(PORT, ()=>{
