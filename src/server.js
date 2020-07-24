880a82f7548e0570aaadfd6fd91b6d1bf51ee88c
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-07-23 21:26:53 -0300

    master
    Criado response

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
 var PORT = process.env.PORT || 3000;
 app.get("/", function (req, res) {
-    res.send("Ta rodando");
+    res.send(JSON.stringify({ a: 'a' }));
 });
 app.listen(PORT, function () {
     console.log("Server running PORT: ", PORT);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 ﻿﻿const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.json())
+
 const PORT = process.env.PORT || 3000
 
 
 app.get("/", (req, res) => {
-    res.send("Ta rodando")
+    res.JSON({"fulfillmentMessages":"ESTA RODANDO NO HEROKU"})
 })
 app.listen(PORT, ()=>{
     console.log("Server running PORT: ", PORT)
