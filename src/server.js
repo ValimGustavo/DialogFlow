258e634cb0759997677f854bcdeb3b0e5f4e27de
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-07-23 21:34:26 -0300

    master
    Corrigido response.json

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 var PORT = process.env.PORT || 3000;
 app.get("/", function (req, res) {
-    res.send(JSON.stringify({ a: 'a' }));
+    res.send({ "fulfillmentMessages": "ESTA RODANDO NO HEROKU" });
 });
 app.listen(PORT, function () {
     console.log("Server running PORT: ", PORT);
--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 const PORT = process.env.PORT || 3000
 
 
 app.get("/", (req, res) => {
-    res.JSON({"fulfillmentMessages":"ESTA RODANDO NO HEROKU"})
+    res.send({"fulfillmentMessages":"ESTA RODANDO NO HEROKU"})
 })
 app.listen(PORT, ()=>{
     console.log("Server running PORT: ", PORT)
