ed844bb4136d67390bd45e15b5191a4197f92108
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-07-24 09:36:56 -0300

    master
    Teste

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 const PORT = process.env.PORT || 3000
 
 app.get("/", (req, res) => {
-    res.send({"fulfillmentMessages":"ESTA RODANDO NO HEROKU"})
+    res.send({"fulfillmentMessages":"Alteraçao"})
 })
 app.listen(PORT, ()=>{
     console.log("Server running PORT: ", PORT)
