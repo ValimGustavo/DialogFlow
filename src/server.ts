2016815797b8747844e447233457d436e8523ab6
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-07-24 10:44:33 -0300

    a

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 const PORT = process.env.PORT || 3000
 
 app.post("/", (req, res) => {
-    console.log(req.body.parameters);
+    console.log(req.body);
     
     res.send({"fulfillmentMessages":"Alteraçao"})
 })
