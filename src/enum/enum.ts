7465ceed67362450d76430d55862c2f7df43735b
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-07-27 20:43:45 -0300

    master
    criado .env

--- src/enum/enum.ts
+++ src/enum/enum.ts
@@ -1,5 +1,2 @@
-﻿﻿enum url {
-    URL_VIDEO_FORMAT= 'https://www.youtube.com/watch?v=',
-    SEARCH_ID_VIDEO_BY_CATEGORY = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyCrtkWROvn-lVFZqLd397jlXayrz6hWhjs&part=id&q='
-}
+﻿﻿
 
