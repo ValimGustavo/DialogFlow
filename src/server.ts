9f55aa3a6a6aae4f450a8832f624d5a45341962f
ValimGustavo <gustavo.souza.valim@gmail.com>
2020-07-27 20:58:58 -0300

    master

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,32 @@
-﻿﻿import { WebHookRequest } from "./interfaces/webHook-request"
-import { WebHookTextResponse } from "./interfaces/webHook-text-response"
-import { call } from './controller/controller'
-const fetch = require('node-fetch');
-const request = require('request');
+﻿﻿import { WebHookRequest } from "./interfaces/webHook-request";
+import { WebHookTextResponse } from "./interfaces/webHook-text-response";
+import { call } from "./controller/controller";
+import { isConstructorDeclaration } from "typescript";
+const fetch = require("node-fetch");
+const request = require("request");
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.post("/", (req, res) => {
+  const webHookRequest: WebHookRequest = req.body;
 
-    const params = req.body.intent;
+  const response = webHookRequest;
 
-    let response = call(params)
-    //GET INTENT 
+  console.log('response -->', response)
+  console.dir('response --->', response)
 
-    //CALL FUNCTION
+  res.json(response);
+});
 
-    res.json(response)
-
-
-
-})
-
-
-
-app.listen(PORT, ()=>{
-    console.log("Server running PORT: ", PORT)
-})
+app.listen(PORT, () => {
+  console.log("Server running PORT: ", PORT);
+});
--- src/services/esporte.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.intentEsporte = void 0;
-function intentEsporte() {
-    console.log('intent');
-    return { a: 'intent esport' };
-}
-exports.intentEsporte = intentEsporte;
